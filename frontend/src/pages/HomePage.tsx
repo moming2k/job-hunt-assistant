61a89c256c1347e86b501a9468bd0ab043d57668
chrischan-stableclub <moming2k@igpsd.com>
2025-01-02 01:34:58 +0000

    update home page

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,9 +1,152 @@
-import React from 'react'
+import React from 'react';
+import { Badge } from '../components/badge'
+import { Divider } from '../components/divider'
+import { Heading, Subheading } from '../components/heading'
+import { Select } from '../components/select'
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '../components/table'
+import { HomeIcon } from '@heroicons/react/20/solid'
 
+const stats = [
+    { name: 'Total Subscribers', stat: '71,897' },
+    { name: 'Avg. Open Rate', stat: '58.16%' },
+    { name: 'Avg. Click Rate', stat: '24.57%' },
+  ]
+
+const pages = [
+{ name: 'Dashboard', href: '#', current: true },
+// { name: 'Project Nero', href: '#', current: true },
+]
+
+
+const jobs = [
+    { id: "1", company: 'Google', title: 'Frontend Developer', keywords: 'React, Tailwind CSS', match_percentage: '80%', last_update: '2 hours ago' },
+    { id: "1", company: 'Google', title: 'Backend Developer', keywords: 'React, Tailwind CSS', match_percentage: '80%', last_update: '3 hours ago' },
+    // More people...
+  ]
+  
+  
 export default function HomePage() {
-  return (
-    <div className="p-4">
-      <h1 className="text-2xl font-bold">Home Page</h1>
+    return (
+      <div>
+        <nav aria-label="Breadcrumb" className="flex">
+      <ol role="list" className="flex space-x-4 rounded-md bg-white px-6 shadow">
+        <li className="flex">
+          <div className="flex items-center">
+            <a href="#" className="text-gray-400 hover:text-gray-500">
+              <HomeIcon aria-hidden="true" className="size-5 shrink-0" />
+              <span className="sr-only">Home</span>
+            </a>
+          </div>
+        </li>
+        {pages.map((page) => (
+          <li key={page.name} className="flex">
+            <div className="flex items-center">
+              <svg
+                fill="currentColor"
+                viewBox="0 0 24 44"
+                preserveAspectRatio="none"
+                aria-hidden="true"
+                className="h-full w-6 shrink-0 text-gray-200"
+              >
+                <path d="M.293 0l22 22-22 22h1.414l22-22-22-22H.293z" />
+              </svg>
+              <a
+                href={page.href}
+                aria-current={page.current ? 'page' : undefined}
+                className="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700"
+              >
+                {page.name}
+              </a>
+            </div>
+          </li>
+        ))}
+      </ol>
+    </nav>
+        
+        <dl className="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
+          {stats.map((item) => (
+            <div key={item.name} className="overflow-hidden rounded-lg bg-white px-4 py-5 shadow sm:p-6">
+              <dt className="truncate text-sm font-medium text-gray-500">{item.name}</dt>
+              <dd className="mt-1 text-3xl font-semibold tracking-tight text-gray-900">{item.stat}</dd>
+            </div>
+          ))}
+        </dl>
+        <div className="border-b border-gray-200 bg-white px-4 py-5 sm:px-6">
+      <div className="-ml-4 -mt-2 flex flex-wrap items-center justify-between sm:flex-nowrap">
+        <div className="ml-4 mt-2">
+          <h3 className="text-base font-semibold text-gray-900">Job Postings</h3>
+        </div>
+        <div className="ml-4 mt-2 shrink-0">
+          <button
+            type="button"
+            className="relative inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            Create new job
+          </button>
+        </div>
+      </div>
+      {/* Table for the job postings */}
+      <div className="mt-8 flow-root">
+        <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+            <table className="min-w-full divide-y divide-gray-300">
+              <thead>
+                <tr>
+                  <th scope="col" className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0">
+                    Name
+                  </th>
+                  <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                    Title
+                  </th>
+                  <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                    Keywords
+                  </th>
+                  <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                    Match %
+                  </th>
+                  <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                    First seen
+                  </th>
+                  <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                    Status
+                  </th>
+                  <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-0">
+                    <span className="sr-only">Apply</span>
+                  </th>
+                  <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-0">
+                    <span className="sr-only">Hide</span>
+                  </th>
+                </tr>
+              </thead>
+              <tbody className="divide-y divide-gray-200">
+                {jobs.map((job) => (
+                  <tr key={job.id}>
+                    <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0">
+                      {job.company}
+                    </td>
+                    <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{job.title}</td>
+                    <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{job.keywords}</td>
+                    <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{job.match_percentage}</td>
+                    <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{job.last_update}</td>
+                    <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">New</td>
+                    <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
+                      <a href="#" className="text-indigo-600 hover:text-indigo-900">
+                        Apply
+                      </a>
+                    </td>
+                    <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
+                      <a href="#" className="text-indigo-600 hover:text-indigo-900">
+                        Hide
+                      </a>
+                    </td>
+                  </tr>
+                ))} 
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+      </div>
+    )
+  }
